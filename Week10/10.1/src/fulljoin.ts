86a6d5bdd819613811504c19b05c1b1369af7e5b
aakashloyar <aakashloyar@gmail.com>
2025-02-03 14:48:06 +0530

    done some table join operation

---
+++ Week10/10.1/src/fulljoin.ts
@@ -0,0 +1,32 @@
+import { Client } from 'pg';
+
+const client = new Client({
+  connectionString: 'postgresql://neondb_owner:npg_Qgk2h5zKoUEM@ep-floral-dawn-a49ekg7n-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require'
+});
+
+async function getUserDetailsWithAddress() {
+    try {
+        await client.connect();
+        const query = `
+            SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+            FROM users u
+            JOIN addresses a ON u.id = a.user_id
+        `;
+        const result = await client.query(query, []);
+
+        if (result.rows.length > 0) {
+            console.log('User and address found:', result.rows[0]);
+            return result.rows[0];
+        } else {
+            console.log('No user or address found with the given ID.');
+            return null;
+        }
+    } catch (err) {
+        console.error('Error during fetching user and address:', err);
+        throw err;
+    } finally {
+        await client.end();
+    }
+}
+
+getUserDetailsWithAddress();
