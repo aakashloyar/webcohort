a1932a66704592c4f0e739b417a18262c548d254
aakashloyar <aakashloyar@gmail.com>
2025-02-03 22:50:11 +0530

    done some actionable docker

---
+++ Week12/12.1/a.js
@@ -0,0 +1,29 @@
+// Define your connection string (replace placeholders with your actual data)
+const { Client } = require('pg');
+const connectionString = 'postgresql://postgres:mysecretpassword@localhost:5431/postgres';
+
+// Create a new client instance with the connection string
+const client = new Client({
+  connectionString: connectionString
+});
+
+// Connect to the database
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack);
+  } else {
+    console.log('connected to the database');
+  }
+});
+
+// Run a simple query (Example: Fetching the current date and time from PostgreSQL)
+client.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res.rows[0]);
+  }
+
+  // Close the connection
+  client.end();
+});
